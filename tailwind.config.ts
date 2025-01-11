0c3942fd0cd9d7792f8e954531dbc1f8fbe58e8e
TamilSelvanKumaravel <tamilselvank09112001@gmail.com>
2025-01-11 11:30:23 +0530

    Create the header section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import flowbite from 'flowbite-react/tailwind';
+
 import type { Config } from "tailwindcss";
 
 export default {
@@ -5,6 +7,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -14,5 +17,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 } satisfies Config;
