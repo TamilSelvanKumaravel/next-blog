cdfff999a329e577b91032352863ef975ecd30ed
TamilSelvanKumaravel <tamilselvank09112001@gmail.com>
2025-01-27 12:38:14 +0530

    clerk theme updated

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ import { FaMoon, FaSun } from "react-icons/fa";
 import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { dark, light } from '@clerk/themes';
+import { neobrutalism, dark } from '@clerk/themes'; // Adjust the path based on your project structure
 
 
 
@@ -47,7 +47,7 @@ export default function Header() {
         <SignedIn>
           <UserButton
             appearance={{
-              baseTheme: theme === "light" ? light : dark,
+              baseTheme: theme === "light" ? neobrutalism : dark,
             }}
             // userProfileUrl='/dashboard?tab=profile'
           />
@@ -81,6 +81,94 @@ export default function Header() {
     </Navbar>
   );
 }
+
+
+
+// "use client";
+// import { Button, Navbar, TextInput } from "flowbite-react";
+// import Link from "next/link";
+// import { AiOutlineSearch } from "react-icons/ai";
+// import { FaMoon, FaSun } from "react-icons/fa";
+// import { usePathname } from "next/navigation";
+// import { useTheme } from "next-themes";
+// import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+// export default function Header() {
+//   const path = usePathname();
+//   const { theme, setTheme } = useTheme();
+
+//   const clerkAppearance = {
+//     variables: {
+//       colorPrimary: theme === "light" ? "#6366f1" : "#8b5cf6", // Example primary color
+//     },
+//     general: {
+//       colorScheme: theme, // Dynamically set light or dark
+//     },
+//   };
+
+//   return (
+//     <Navbar className="border-b-2">
+//       <Link
+//         href="/"
+//         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+//       >
+//         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+//           Sahand&apos;s
+//         </span>
+//         Blog
+//       </Link>
+//       <form>
+//         <TextInput
+//           type="text"
+//           placeholder="Search..."
+//           rightIcon={AiOutlineSearch}
+//           className="hidden lg:inline"
+//         />
+//       </form>
+//       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+//         <AiOutlineSearch />
+//       </Button>
+//       <div className="flex gap-2 md:order-2">
+//         <Button
+//           className="w-12 h-10 hidden sm:inline"
+//           color="gray"
+//           pill
+//           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+//         >
+//           {theme === "light" ? <FaSun /> : <FaMoon />}
+//         </Button>
+//         <SignedIn>
+//           <UserButton appearance={clerkAppearance} />
+//         </SignedIn>
+//         <SignedOut>
+//           <Link href="/sign-in">
+//             <Button gradientDuoTone="purpleToBlue" outline as="div">
+//               Sign In
+//             </Button>
+//           </Link>
+//         </SignedOut>
+//         <Navbar.Toggle />
+//       </div>
+//       <Navbar.Collapse>
+//         <Link href="/">
+//           <Navbar.Link active={path === "/"} as={"div"}>
+//             Home
+//           </Navbar.Link>
+//         </Link>
+//         <Link href="/about">
+//           <Navbar.Link active={path === "/about"} as={"div"}>
+//             About
+//           </Navbar.Link>
+//         </Link>
+//         <Link href="/projects">
+//           <Navbar.Link active={path === "/projects"} as={"div"}>
+//             Projects
+//           </Navbar.Link>
+//         </Link>
+//       </Navbar.Collapse>
+//     </Navbar>
+//   );
+// }
 
 
 // 'use client';
