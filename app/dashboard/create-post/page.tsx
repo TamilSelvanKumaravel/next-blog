e966c7e23caabc555138860f4c4e060a0b4e7306
TamilSelvanKumaravel <tamilselvank09112001@gmail.com>
2025-01-30 11:36:34 +0530

    complete upload post image functionality using cloudinary

--- app/dashboard/create-post/page.tsx
+++ app/dashboard/create-post/page.tsx
@@ -1,15 +1,135 @@
+// 'use client';
+
+// import { useUser } from '@clerk/nextjs';
+// import { Button, FileInput, Select, TextInput } from 'flowbite-react';
+
+// import dynamic from 'next/dynamic';
+// const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
+// // https://dev.to/a7u/reactquill-with-nextjs-478b
+// import 'react-quill-new/dist/quill.snow.css';
+
+// export default function CreatePostPage() {
+//   const { isSignedIn, user, isLoaded } = useUser();
+
+//   if (!isLoaded) {
+//     return null;
+//   }
+
+//   if (isSignedIn && user.publicMetadata.isAdmin) {
+//     return (
+//       <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+//         <h1 className='text-center text-3xl my-7 font-semibold'>
+//           Create a post
+//         </h1>
+//         <form className='flex flex-col gap-4'>
+//           <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+//             <TextInput
+//               type='text'
+//               placeholder='Title'
+//               required
+//               id='title'
+//               className='flex-1'
+//             />
+//             <Select>
+//               <option value='uncategorized'>Select a category</option>
+//               <option value='javascript'>JavaScript</option>
+//               <option value='reactjs'>React.js</option>
+//               <option value='nextjs'>Next.js</option>
+//             </Select>
+//           </div>
+//           <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+//             <FileInput accept='image/*' />
+//             <Button
+//               type='button'
+//               gradientDuoTone='purpleToBlue'
+//               size='sm'
+//               outline
+//             >
+//                 Upload Image
+//             </Button>
+//           </div>
+
+//           <ReactQuill
+//             theme='snow'
+//             placeholder='Write something...'
+//             className='h-72 mb-12'
+//           />
+//           <Button type='submit' gradientDuoTone='purpleToPink'>
+//             Publish
+//           </Button>
+//         </form>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <h1 className='text-center text-3xl my-7 font-semibold'>
+//         You are not authorized to view this page
+//       </h1>
+//     );
+//   }
+// }
+
+
 'use client';
 
 import { useUser } from '@clerk/nextjs';
-import { Button, FileInput, Select, TextInput } from 'flowbite-react';
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
 const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
 // https://dev.to/a7u/reactquill-with-nextjs-478b
 import 'react-quill-new/dist/quill.snow.css';
 
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 export default function CreatePostPage() {
   const { isSignedIn, user, isLoaded } = useUser();
+
+  const [file, setFile] = useState<File | null>(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState<number | null>(null);
+  const [imageUploadError, setImageUploadError] = useState<string | null>(null);
+  const [formData, setFormData] = useState<{ image?: string }>({});
+
+
+  const handleUploadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError('Please select an image');
+        return;
+      }
+      setImageUploadError(null);
+
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!); // Replace with your Cloudinary preset
+      formData.append('cloud_name', process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!); // Replace with your Cloudinary cloud name
+
+      const res = await fetch(
+        `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+        {
+          method: 'POST',
+          body: formData,
+        }
+      );
+
+      const data = await res.json();
+
+      if (data.secure_url) {
+        setImageUploadProgress(null);
+        setImageUploadError(null);
+        setFormData({ ...formData, image: data.secure_url });
+      } else {
+        setImageUploadError('Image upload failed');
+        setImageUploadProgress(null);
+      }
+    } catch (error) {
+      setImageUploadError('Image upload failed');
+      setImageUploadProgress(null);
+      console.error(error);
+    }
+  };
 
   if (!isLoaded) {
     return null;
@@ -18,18 +138,10 @@ export default function CreatePostPage() {
   if (isSignedIn && user.publicMetadata.isAdmin) {
     return (
       <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-        <h1 className='text-center text-3xl my-7 font-semibold'>
-          Create a post
-        </h1>
+        <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
         <form className='flex flex-col gap-4'>
           <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-            <TextInput
-              type='text'
-              placeholder='Title'
-              required
-              id='title'
-              className='flex-1'
-            />
+            <TextInput type='text' placeholder='Title' required id='title' className='flex-1' />
             <Select>
               <option value='uncategorized'>Select a category</option>
               <option value='javascript'>JavaScript</option>
@@ -38,33 +150,29 @@ export default function CreatePostPage() {
             </Select>
           </div>
           <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
-            <FileInput accept='image/*' />
-            <Button
-              type='button'
-              gradientDuoTone='purpleToBlue'
-              size='sm'
-              outline
-            >
-                Upload Image
+            <FileInput accept='image/*' onChange={(e) => {if (e.target.files && e.target.files.length > 0) {
+              setFile(e.target.files[0]);
+              }}} />
+            <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline onClick={handleUploadImage}>
+              {imageUploadProgress ? (
+                <div className='w-16 h-16'>
+                  <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`} />
+                </div>
+              ) : (
+                'Upload Image'
+              )}
             </Button>
           </div>
 
-          <ReactQuill
-            theme='snow'
-            placeholder='Write something...'
-            className='h-72 mb-12'
-          />
-          <Button type='submit' gradientDuoTone='purpleToPink'>
-            Publish
-          </Button>
+          {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+          {formData.image && <img src={formData.image} alt='upload' className='w-full h-72 object-cover' />}
+
+          <ReactQuill theme='snow' placeholder='Write something...' className='h-72 mb-12' />
+          <Button type='submit' gradientDuoTone='purpleToPink'>Publish</Button>
         </form>
       </div>
     );
   } else {
-    return (
-      <h1 className='text-center text-3xl my-7 font-semibold'>
-        You are not authorized to view this page
-      </h1>
-    );
+    return <h1 className='text-center text-3xl my-7 font-semibold'>You are not authorized to view this page</h1>;
   }
 }
