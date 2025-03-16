949a8d2850180e1af68ce96047b17cd2aafcd249
TamilSelvanKumaravel <tamilselvank09112001@gmail.com>
2025-03-16 13:22:56 +0530

    General updates like Title,Header and Footer etc..

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -20,7 +20,7 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Sahand&apos;s
+              Techie&apos;s
               </span>
               Blog
             </Link>
@@ -41,7 +41,7 @@ export default function FooterCom() {
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Sahand&apos;s Blog
+                  Techie&apos;s Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -49,13 +49,13 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://github.com/Veeramuthubsc'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
                   Github
                 </Footer.Link>
-                <Footer.Link href='#'>Discord</Footer.Link>
+                <Footer.Link href='https://www.instagram.com/_muthu_26__/?utm_source=qr&igsh=NnJ3ZzI0c3oycGoz' target='_blank'>Insta</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
@@ -70,16 +70,17 @@ export default function FooterCom() {
         <Footer.Divider />
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
-            href='#'
-            by="Sahand's blog"
+            href='/'
+            by="Techie's blog"
             year={new Date().getFullYear()}
           />
           <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
             <Footer.Icon href='#' icon={BsFacebook} />
-            <Footer.Icon href='#' icon={BsInstagram} />
+            <Footer.Icon href='https://www.instagram.com/_muthu_26__/?utm_source=qr&igsh=NnJ3ZzI0c3oycGoz' target='_blank' icon={BsInstagram} />
             <Footer.Icon href='#' icon={BsTwitter} />
             <Footer.Icon
-              href='https://github.com/sahandghavidel'
+              href='https://github.com/Veeramuthubsc'
+              target='_blank'
               icon={BsGithub}
             />
             <Footer.Icon href='#' icon={BsDribbble} />
